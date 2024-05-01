c619c15963d4e24e65766eb93df5ef828ce3bdf9
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2024-05-01 17:18:58 -0300

    feat: add category products page

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
 import { ArrowDownIcon } from "lucide-react";
 import Link from "next/link";
+import { cn } from "../_lib/utils";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
@@ -14,13 +15,17 @@ interface ProductItemProps {
       };
     };
   }>;
+  className?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, className }: ProductItemProps) => {
   return (
-    <Link className="w-[150px] min-w-[150px]" href={`/products/${product.id}`}>
+    <Link
+      className={cn("w-[150px] min-w-[150px]", className)}
+      href={`/products/${product.id}`}
+    >
       <div className="w-full space-y-2">
-        <div className="relative h-[150px] w-full">
+        <div className="relative aspect-square w-full">
           <Image
             src={product.imageUrl}
             alt={product.name}
@@ -28,7 +33,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
             className="rounded-lg object-cover shadow-md"
           />
 
-          {product.discountPercentage && (
+          {product.discountPercentage > 0 && (
             <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary px-2 py-[2px] text-white">
               <ArrowDownIcon size={12} />
               <span className="text-xs font-semibold">
@@ -44,7 +49,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
             <h3 className="font-semibold">
               {formatCurrency(calculateProductTotalPrice(product))}
             </h3>
-            {product.discountPercentage && (
+            {product.discountPercentage > 0 && (
               <span className="text-xs text-muted-foreground line-through">
                 {formatCurrency(Number(product.price))}
               </span>
