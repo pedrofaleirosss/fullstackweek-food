f035dbd0443728396f4e657c00a5f34327c0f9b3
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2024-05-03 23:31:48 -0300

    feat: add orders page

--- app/_components/cart.tsx
+++ app/_components/cart.tsx
@@ -49,6 +49,14 @@ const Cart = () => {
         user: {
           connect: { id: data.user.id },
         },
+        products: {
+          createMany: {
+            data: products.map((product) => ({
+              productId: product.id,
+              quantity: product.quantity,
+            })),
+          },
+        },
       });
 
       clearCart();
